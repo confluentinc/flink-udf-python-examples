91ee1964d70cc81857d62d18b8dee491a58ce17c
David Selassie <dselassie@confluent.io>
2025-09-23 16:55:21 -0700

    Uses UDF declaration that passes mypy and lets you test underlying function

--- src/example_udf/scalar.py
+++ src/example_udf/scalar.py
@@ -1,18 +1,33 @@
 from pyflink.table import DataTypes
+from pyflink.table.types import DataType
 from pyflink.table.udf import udf
 
 
-@udf(
-    input_types=[DataTypes.BIGINT(nullable=False), DataTypes.BIGINT(nullable=False)],
+def _f_int_add(i: int, j: int) -> int:
+    return i + j
+
+
+_int_add_inp_types: list[DataType] = [
+    DataTypes.BIGINT(nullable=False),
+    DataTypes.BIGINT(nullable=False),
+]
+int_add = udf(
+    _f_int_add,
+    input_types=_int_add_inp_types,
     result_type=DataTypes.BIGINT(nullable=False),
 )
-def int_add(i: int, j: int) -> int:
+
+
+def _f_str_concat(i: str, j: str) -> str:
     return i + j
 
 
-@udf(
-    input_types=[DataTypes.VARCHAR(nullable=False), DataTypes.VARCHAR(nullable=False)],
-    result_type=DataTypes.VARCHAR(nullable=False),
+_str_concat_inp_types: list[DataType] = [
+    DataTypes.STRING(nullable=False),
+    DataTypes.STRING(nullable=False),
+]
+str_concat = udf(
+    _f_str_concat,
+    input_types=_str_concat_inp_types,
+    result_type=DataTypes.STRING(nullable=False),
 )
-def str_concat(i: str, j: str) -> str:
-    return i + j
